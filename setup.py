3752bb9d33c82e4526a44c78ef5f0cbea0395354
luav <luardev@gmail.com>
2018-05-01 19:05:50 +0200

    Package setup refined

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
 	'(like in [PyCaBeM](https://github.com/eXascaleInfolab/PyCABeM)), not as a'
 	' separate library. Additionally, an optional minimalistic Web interface is'
 	' provided in the separate file to inspect the load balancer and execution pool.'),
+	long_description_content_type='text/markdown',
 	url='https://github.com/eXascaleInfolab/PyExPool',
 	author='Artem Lutov',
 	author_email='artem@exascale.info',
@@ -99,6 +100,8 @@ setup(
 	#
 	# py_modules=["my_module"],
 	# packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # Required
+	# packages=['pyexpool'],
+	# packages=['__init__'],
 	py_modules=['mpepool', 'mpewui'],
 
 	# This field lists other packages that your project depends on to run.
@@ -124,21 +127,28 @@ setup(
 		'test': ['mock'],  # Only for Python 2
 	},
 
+	#package_dir={'pyexpool': '.'},
+
 	# If there are data files included in your packages that need to be
 	# installed, specify them here.
 	#
 	# If using Python 2.6 or earlier, then these have to be included in
 	# MANIFEST.in as well.
-	# package_data={  # Optional
-	# 	'sample': ['package_data.dat'],
-	# },
+	package_data={  # Optional
+		# 'sample': ['package_data.dat'],
+		'pyexpool': ['README.md', 'images/*'],
+		# '.': ['README.md', 'images/*']
+	},
 
 	# Although 'package_data' is the preferred approach, in some case you may
 	# need to place data files outside of your packages. See:
 	# http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
 	#
 	# In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-	# data_files=[('my_data', ['data/data_file'])],  # Optional
+	# data_files specifies a sequence of (directory, files) pairs in the following way
+	# data_files=[('', ['README.md', 'images/*']),
+	# 	('images', ['images/*']),
+	# ],  # Optional
 
 	# To provide executable scripts, use entry points in preference to the
 	# "scripts" keyword. Entry points provide cross-platform support and allow
